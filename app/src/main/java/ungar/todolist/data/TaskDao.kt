af84f7dc6d7a5f06403d7fa31fe5377b80b8c3f7
Borna Ungar <borna.ungar@gmail.com>
2021-01-01 18:31:31 +0100

    combining multiple flows (sort by)

--- app/src/main/java/ungar/todolist/data/TaskDao.kt
+++ app/src/main/java/ungar/todolist/data/TaskDao.kt
@@ -2,14 +2,26 @@ package ungar.todolist.data
 
 import androidx.room.*
 import kotlinx.coroutines.flow.Flow
+import ungar.todolist.ui.tasks.SortOrder
 
 //Data Access Object
 //Interface for various database method that we'll use
 @Dao
 interface TaskDao {
 
-    @Query("SELECT * FROM task_table WHERE name LIKE '%' || :searchQuery || '%' ORDER BY important DESC")
-    fun getTasks(searchQuery: String): Flow<List<Task>>
+    fun getTasks(query: String, sortOrder: SortOrder, hideCompleted: Boolean): Flow<List<Task>> =
+        when(sortOrder){
+            SortOrder.BY_DATE -> getTasksSortedByDateCreated(query,hideCompleted)
+            SortOrder.BY_NAME -> getTasksSortedByName(query, hideCompleted)
+        }
+
+    @Query("SELECT * FROM task_table WHERE (completed != :hideCompleted OR completed = 0) AND name LIKE '%' || :searchQuery || '%' ORDER BY important DESC, name")
+    fun getTasksSortedByName(searchQuery: String, hideCompleted: Boolean): Flow<List<Task>>
+
+    @Query("SELECT * FROM task_table WHERE (completed != :hideCompleted OR completed = 0) AND name LIKE '%' || :searchQuery || '%' ORDER BY important DESC, created")
+    fun getTasksSortedByDateCreated(searchQuery: String, hideCompleted: Boolean): Flow<List<Task>>
+
+
     // flow is an asynchronious stream of data
     // flow vs livedata
 
