ef3822286ce23b0ddfb981550a7d35b0e6c353c4
Borna Ungar <borna.ungar@gmail.com>
2021-01-01 16:07:16 +0100

    listadapter w/ viewbinding

--- app/src/main/java/ungar/todolist/ui/tasks/TasksFragment.kt
+++ app/src/main/java/ungar/todolist/ui/tasks/TasksFragment.kt
@@ -1,12 +1,37 @@
 package ungar.todolist.ui.tasks
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import dagger.hilt.android.AndroidEntryPoint
 import ungar.todolist.R
+import ungar.todolist.databinding.FragmentTasksBinding
 
 @AndroidEntryPoint
 class TasksFragment: Fragment(R.layout.fragment_tasks) {
 
-    private val viedModel: TasksViewModel by viewModels()
+    private val viewModel: TasksViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentTasksBinding.bind(view)
+
+        val taskAdapter = TasksAdapter()
+
+        binding.apply {
+            recyclerViewTasks.apply {
+                adapter = taskAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+                setHasFixedSize(true)
+            }
+        }
+
+        viewModel.tasks.observe(viewLifecycleOwner) {
+            taskAdapter.submitList(it)
+        }
+    }
 }
--- app/src/main/java/ungar/todolist/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/ungar/todolist/ui/tasks/TasksViewModel.kt
@@ -2,9 +2,12 @@ package ungar.todolist.ui.tasks
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import ungar.todolist.data.TaskDao
 
 class TasksViewModel @ViewModelInject constructor(
     private val taskDao: TaskDao
 ) : ViewModel() {
+
+    val tasks = taskDao.getTasks().asLiveData()
 }
